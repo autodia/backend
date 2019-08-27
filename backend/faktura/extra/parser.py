003b09975541df0c783e08a09353aba283bfc0ff
Crosan <cough12@hotmail.com>
2019-08-27 09:41:47 +0200

    Update parser.py

--- backend/faktura/extra/parser.py
+++ backend/faktura/extra/parser.py
@@ -7,7 +7,7 @@ import math
 
 from django.core.management.base import BaseCommand, CommandError
 from backend.faktura.models import *
-from datetime import timedelta
+from datetime import timedelta, datetime
 from django.core.management import call_command
 from django.conf import settings
 from dateutil.relativedelta import relativedelta
